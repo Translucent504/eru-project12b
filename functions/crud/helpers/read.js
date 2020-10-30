8541e5b938cb7b7d8d6764bff21df0912eafd5e6
Translucent504 <fleshlime.1@gmail.com>
2020-10-30 22:27:18 +0500

    netlify functions helper to read documents from faunadb

---
+++ functions/crud/helpers/read.js
@@ -0,0 +1,18 @@
+const { Client, query: q } = require('faunadb')
+
+module.exports = async function readAllContacts() {
+    const client = new Client({
+        secret: process.env.FAUNA_SECRET
+    })
+    const result = await client.query(
+        q.Map(
+            q.Paginate(
+                q.Match(
+                    q.Index("all_contacts")
+                )
+            ),
+            q.Lambda("X", q.Get(q.Var("X")))
+        )
+    )
+    return result.data.map(r => r.data)
+}
