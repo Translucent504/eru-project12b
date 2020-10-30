06a1ce46c5dd614b471d5d8f42837c086b878283
Translucent504 <fleshlime.1@gmail.com>
2020-10-30 22:27:05 +0500

    netlify functions helper to create document in faunadb

---
+++ functions/crud/helpers/create.js
@@ -0,0 +1,12 @@
+const { Client, query: q } = require('faunadb')
+
+module.exports = function createContact(userinfo) {
+    console.log('creating',userinfo)
+    const client = new Client({
+        secret: process.env.FAUNA_SECRET
+    })
+    client.query(q.Create(
+        q.Collection('contacts'),
+        { data: userinfo })
+    ).then(v => console.log(v))
+}
